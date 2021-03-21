4c2f394f4f9e31ffbf35331304e15615da1711e2
Sweet Acid <antoniu.ilea@gmail.com>
2021-03-21 23:46:08 +0200

    Added content display with image and text

--- src/app/editor/editor.module.ts
+++ src/app/editor/editor.module.ts
@@ -5,9 +5,10 @@ import { EditorRoutingModule } from './editor-routing.module';
 import { EditorComponent } from './editor.component';
 import { NodeComponent } from './tree/node/node.component';
 import { TreeComponent } from './tree/tree.component';
+import { ImageTextComponent } from './panels/image-text/image-text.component';
 
 @NgModule({
-  declarations: [EditorComponent, NodeComponent, TreeComponent],
+  declarations: [EditorComponent, NodeComponent, TreeComponent, ImageTextComponent],
   imports: [CommonModule, SharedModule, EditorRoutingModule]
 })
 export class EditorModule { }
---
+++ src/app/editor/panels/image-text/image-text.component.ts
@@ -0,0 +1,42 @@
+import {
+    AfterViewInit,
+    Component,
+    ElementRef,
+    HostListener,
+    Input,
+
+    ViewChild
+} from "@angular/core";
+
+@Component({
+    selector: "app-image-text",
+    templateUrl: "./image-text.component.html",
+    styleUrls: ["./image-text.component.scss"],
+})
+export class ImageTextComponent implements AfterViewInit {
+    @Input() public title: string;
+    @Input() public content: string;
+    @Input() public image: string;
+
+    constructor() {}
+
+    ngAfterViewInit(): void {
+        this.recalculateContainerSize();
+    }
+
+    @ViewChild("header")
+    public headerElem: ElementRef<HTMLDivElement>;
+
+    @ViewChild("container")
+    public contentElem: ElementRef<HTMLDivElement>;
+
+    @HostListener("window:resize")
+    public onResize() {
+        this.recalculateContainerSize();
+    }
+
+    private recalculateContainerSize() {
+        let header_height = this.headerElem.nativeElement.clientHeight;
+        this.contentElem.nativeElement.style.height = `calc(100% - ${header_height}px)`;
+    }
+}
