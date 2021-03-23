87a4dc40d9f4221377fdc8619058b7783d64c712
Sweet Acid <antoniu.ilea@gmail.com>
2021-03-23 20:10:16 +0200

    Content display components' heights scales with parent's height and have overflow

--- src/app/editor/content/image-text/image-text.component.ts
+++ src/app/editor/content/image-text/image-text.component.ts
@@ -1,35 +1,14 @@
-import {
-    AfterViewInit,
-    Component,
-    ElementRef,
-    Input,
-    ViewChild
-} from "@angular/core";
+import { Component, Input } from "@angular/core";
 
 @Component({
     selector: "content-image-text",
     templateUrl: "./image-text.component.html",
     styleUrls: ["./image-text.component.scss"],
 })
-export class ImageTextComponent implements AfterViewInit {
+export class ImageTextComponent {
     @Input() public title: string;
     @Input() public content: string;
     @Input() public image: string;
 
     constructor() {}
-
-    ngAfterViewInit(): void {
-        this.recalculateContainerSize();
-    }
-
-    @ViewChild("header")
-    public headerElem: ElementRef<HTMLDivElement>;
-
-    @ViewChild("container")
-    public contentElem: ElementRef<HTMLDivElement>;
-
-    private recalculateContainerSize() {
-        let header_height = this.headerElem.nativeElement.clientHeight;
-        this.contentElem.nativeElement.style.height = `calc(100% - ${header_height}px)`;
-    }
 }
--- src/app/editor/content/text/text.component.ts
+++ src/app/editor/content/text/text.component.ts
@@ -1,35 +1,13 @@
-import {
-    AfterViewChecked,
-
-    Component,
-    ElementRef,
-    Input,
-    ViewChild
-} from "@angular/core";
+import { Component, Input } from "@angular/core";
 
 @Component({
     selector: "content-text",
     templateUrl: "./text.component.html",
     styleUrls: ["./text.component.scss"],
 })
-export class TextComponent implements AfterViewChecked {
+export class TextComponent {
     @Input() public title: string;
     @Input() public content: string;
 
     constructor() {}
-
-    ngAfterViewChecked(): void {
-        this.recalculateContainerSize();
-    }
-
-    @ViewChild("header")
-    public headerElem: ElementRef<HTMLDivElement>;
-
-    @ViewChild("container")
-    public contentElem: ElementRef<HTMLDivElement>;
-
-    private recalculateContainerSize() {
-        let header_height = this.headerElem.nativeElement.clientHeight;
-        this.contentElem.nativeElement.style.height = `calc(100% - ${header_height}px)`;
-    }
 }
